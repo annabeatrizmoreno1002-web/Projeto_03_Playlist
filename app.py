2b2906a512985b18c8c11ed35c46f6d5f45a9cc9
annabeatrizmoreno1002-web <annabeatrizmoreno1002@gmail.com>
2026-06-18 10:37:48 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,21 @@
+import streamlit as st 
+
+generos = {
+    'Trap' : {
+        'Matuê' : 'https://www.youtube.com/watch?v=ZPcG9PCfagM',
+        'Teto' : 'https://www.youtube.com/watch?v=7AlAYttGnAg'
+    },
+    'Sertanejo' : {
+        'Gustavo Lima' : 'https://www.youtube.com/watch?v=hEBjnXXWgKU',
+        'Renner Azevedo' : 'https://www.youtube.com/watch?v=AxGv4RG9DqY'
+    },
+    'Pagode' : {
+        'Zeca Pagodinho' : 'https://www.youtube.com/watch?v=oTREAvZbmME',
+        'Tiaguinho' : 'https://www.youtube.com/watch?v=qUqc_cYejX0'
+    },
+    'Eletronica' : {
+        'Alok' : 'https://www.youtube.com/watch?v=JVpTp8IHdEg',
+        'Calvin Harris' : 'https://www.youtube.com/watch?v=ebXbLfLACGM'
+    }
+}
+
